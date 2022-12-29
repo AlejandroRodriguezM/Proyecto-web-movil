f8f18e7d3a9de23fc66a5fd776176c0907e85435
antoniabal <abalvaz@yahoo.es>
2022-12-29 13:36:24 +0100

    estilo-insertar-gestionar

---
+++ postindex.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/style/stylePostindex.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+    <title>Layered Card Hover</title>
+</head>
+<body>
+    <section class="hero-section">
+        <div class="card-grid">
+            <a class="card" href="#">
+                <div class="card__background" style="background-image: url('assets/img/insertar.jpg')"></div>
+                <div class="card__content">
+                    <h3 class="card__heading">Insertar</h3>
+                </div>
+            </a>
+            <a class="card" href="#">
+                <div class="card__background" style="background-image: url('assets/img/gestionar.jpg')"></div>
+                <div class="card__content">
+                    <h3 class="card__heading">Gestionar</h3>
+                </div>
+            </a>
+        </div>
+    </section>
+</body>
+</html>
